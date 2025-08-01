38cb12aaeaddd878b17ebf7916914c1d8535ccc5
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-08-01 09:51:34 +0700

    Add mysql

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import router from './routes/web.js';
 // const { default: configViewEngine } = require('./config/viewEngine');
 import configViewEngine from './config/viewEngine.js'
-
+import mysql from 'mysql2';
 
 
 config();
@@ -15,7 +15,26 @@ const hostname = process.env.HOST_NAME;
 configViewEngine(app);
 
 
+
+
+
 app.use(router);
+
+const connection = mysql.createConnection({
+      host: process.env.DATABASE_HOST, // Or your Docker host IP
+      user: process.env.DATABASE_USER, // Or your defined user
+      password: process.env.DATABASE_PASSWORD, // Or your defined password
+      database: process.env.DATABASE_NAME, // If you created one
+      port: process.env.DATABASE_PORT,
+    });
+
+connection.query(
+  'SELECT * FROM Users',
+  function (err, results, fields) {
+    console.log(">>> Result: " ,results); // results contains rows returned by server
+    console.log(">>> Fields: " ,fields); // fields contains extra meta data about results, if available
+  }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
